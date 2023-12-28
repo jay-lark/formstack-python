97be4c3c9efa86bb6125fd1c9a0b74910999c16e
Jay Lark <jay@jaylark.com>
2023-12-28 16:06:01 -0500

    add deletion test

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -6,7 +6,7 @@ oauth_token = os.environ["OAUTH_TOKEN"]
 fs = FormsClient(hostname=os.environ["API_URL"], token=oauth_token)
 
 
-def test_create_and_get_form():
+def test_create_get_delete_form():
     form_data = {
         "db": False,
         "label_position": "top",
@@ -25,3 +25,6 @@ def test_create_and_get_form():
     assert get_form["name"] == "this is a test form"
     assert get_form["created"] == form_create["created"]
     assert get_form["id"] == form_create["id"]
+    form_delete = fs.delete(f"form/{form_id}")
+    assert form_delete["id"] == form_create["id"]
+    assert form_delete["success"] == 1
